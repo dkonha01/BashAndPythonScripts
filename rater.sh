defdfa39e50dd80c03228d94f47fd104a1f1da25
Daniel Konhauser <dkonhauser@gmail.com>
2021-03-10 11:05:32 -0800

    Update rater.sh
    added prompts for Source and Destination folders
    
    TODO -add desired bit rate cutoff

--- rater.sh
+++ rater.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-for i in *.mp3
+echo -n "Enter the source folder: "
+read src
+echo -n "Enter the destination folder: "
+read dest
+for i in "$src"/*.mp3
 do
      brate=$(mediainfo "$i" | grep "Bit rate     "  | awk '{print $4}')
-     if [  "$brate" -gt 160 ] && ! [ -f Selects/"i" ]; then
-         cp "$i" Selects
-
+     if [  "$brate" -gt 160 ] && ! [ -f "$dest"/"i" ]; then
+         cp "$i" "$dest" 
          #  echo "$i:$brate" 
      fi
 done
