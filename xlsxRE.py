0ef7ce86e688700c153716d94d520ea109f0f4c3
Daniel Konhauser <dkonhauser@gmail.com>
2021-03-09 17:00:47 -0800

    Update xlsxRE.py
    adding CLI input

--- xlsxRE.py
+++ xlsxRE.py
@@ -2,6 +2,7 @@ from openpyxl import *
 import sys
 import re
 
+inputString = sys.argv[2]
 
 wb = load_workbook(sys.argv[1])
 sheet = wb.active
@@ -9,7 +10,7 @@ sheet = wb.active
 for row in sheet.iter_rows(6):
     for cell in row:
         cv = cell.value
-        pattern = '^King\sTubby+'
+        pattern = '^inputString'
         m = re.search(pattern, str(cv))
         if m:
             print(sheet.cell(row=cell.row, column=6).value)
