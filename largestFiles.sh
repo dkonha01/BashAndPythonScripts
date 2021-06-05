5a9695cf1002130ace2625961c43e27fc18a4480
Daniel Konhauser <dkonhauser@gmail.com>
2021-06-05 16:31:13 -0700

    Find largest files in all subdirectories

--- largestFiles.sh
+++ largestFiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Find largest files %s - size - %p -filename
+# Find largest files in all subdirectories %s - size - %p -filename
 
 sudo find / -type f -printf "%s\t%p\n" 2>/dev/null | sort -n | tail -10
